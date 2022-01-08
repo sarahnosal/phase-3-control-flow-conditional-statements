2b28684e144abf5863943a7ba38686c218ed262e
Sarah Nosal <nosalsm@gmail.com>
2022-01-07 22:04:46 -0600

    Completed Assignment

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,40 @@
 def admin_login(username, password)
-  # your code here
+  if (username == 'admin' || username == 'ADMIN') && password == '12345' 
+    'Access granted'
+  else 
+    'Access denied'
+  end
 end
 
-def hows_the_weather(temperature)
-  # your code here
+def hows_the_weather(temperature) 
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature >= 40 && temperature < 65
+    "It's a little chilly out there!"
+  elsif temperature > 85
+    "It's too dang hot out there!"
+  else 
+    "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 && num % 5 == 0
+    num = "FizzBuzz"
+  elsif num % 5 == 0
+    num = "Buzz"
+  elsif num % 3 == 0 
+    num = 'Fizz'
+  else
+    num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  if (operation == '+' || operation == '-' || operation == '*' || operation == '/')
+    num1.send(operation, num2)
+  else 
+    puts 'Invalid operation!'
+  end
 end
 
